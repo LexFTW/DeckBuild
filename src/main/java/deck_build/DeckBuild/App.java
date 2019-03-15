e2a1c13eeebd07461558c493eca2f272d9bfe17b
LexFTW <lexmengual@gmail.com>
2019-03-15 19:24:53 +0100

    15/03/2019 19:24

---
+++ src/main/java/deck_build/DeckBuild/App.java
@@ -0,0 +1,13 @@
+package deck_build.DeckBuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
